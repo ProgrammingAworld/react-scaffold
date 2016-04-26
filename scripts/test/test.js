19f3872efdc9edcfa27d67f7f4aa4ea5e879d4db
安 <pvfhv@qq.com>
2016-04-26 15:46:27 +0800

    fix
    fix

---
+++ scripts/test/test.js
@@ -0,0 +1,12 @@
+/**
+ * Created by anchao on 2016/2/2.
+ */
+import jest from 'jest-cli';
+
+class Test{
+    simulate(){
+        let fn = jest.genMockFunction();
+    }
+}
+
+export default Test;
