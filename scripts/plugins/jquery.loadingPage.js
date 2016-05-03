014303afdaee9dbd672aa128ff8ed9201cac132b
安 <pvfhv@qq.com>
2016-05-03 19:33:02 +0800

    fix
    fix

--- scripts/plugins/jquery.loadingPage.js
+++ scripts/plugins/jquery.loadingPage.js
@@ -8,7 +8,7 @@
     function LoadingPage(actionName) {
         this.ver = '1.0';
         this.actionName = actionName;
-        this.done = false;
+        this.finish = false;
         this.loadEle = null;
         this.spanTime = 300;
         this.HTML = '<div id="loading"><div class="progress"><div role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="min-width: 2em;" class="progress-bar progress-bar-success progress-bar-striped active">0%</div></div></div>';
@@ -18,7 +18,7 @@
     LoadingPage.prototype = {
         constructor: LoadingPage,
         init: function () {
-            if(this.loadEle == null){
+            if (this.loadEle == null) {
                 var html = this.HTML;
                 $(document.body).prepend(html);
                 this.loadEle = $('#loading');
@@ -32,24 +32,34 @@
                     this.stop();
                     break;
                 case 'done':
-                    this.done = true;
-                    this.spanTime = 30;
-                    this.start();
+                    this.done();
                     break;
                 default:
                     break;
             }
         },
-        setActionName:function (actionName) {
+        setActionName: function (actionName) {
             this.actionName = actionName;
         },
         start: function () {
             this.stop();
             var spanTime = this.spanTime;
-            this.loadEle.data('loadingtimer',setInterval(this.animate.bind(this),spanTime));
+            this.loadEle.data('loadingtimer', setInterval(this.animate.bind(this), spanTime));
         },
         stop: function () {
             clearInterval(this.loadEle.data('loadingtimer'));
+        },
+        done: function () {
+            this.stop();
+            this.loadEle.find('.progress-bar').attr('aria-valuenow', 100).css('width', '100%').text('100%');
+            this.finish = true;
+
+            //结束画面停顿一下
+            setTimeout(function () {
+                this.loadEle.animateCss('fadeOutUp', function () {
+                    $(this).addClass('hide');
+                });
+            }.bind(this), 1000);
         },
         animate: function () {
             var $progressbar = this.loadEle.find('.progress-bar');
@@ -58,30 +68,22 @@
             $progressbar.attr('aria-valuenow', nValue).css('width', nValue + '%').text(nValue + '%');
 
             //手动暂停，避免页面数据还求加载完成,loading完成
-            if (nValue == 96&&!this.done) {
+            if (nValue == 96 && !this.finish) {
                 this.stop();
             }
 
             if (nValue == 100) {
-                this.done = true;
-                this.stop();
-
-                //结束画面停顿一下
-                setTimeout(function () {
-                    this.loadEle.animateCss('fadeOutUp', function () {
-                        $(this).addClass('hide');
-                    });
-                }.bind(this),800);
+                this.done();
             }
         }
     };
 
     $.fn.loadingPage = function (actionName) {
         var instance = this.data('loadingpage');
-        if(!instance){
+        if (!instance) {
             instance = new LoadingPage(actionName);
-            this.data('loadingpage',instance);
-        }else {
+            this.data('loadingpage', instance);
+        } else {
             instance.setActionName(actionName);
             instance.init();
         }
