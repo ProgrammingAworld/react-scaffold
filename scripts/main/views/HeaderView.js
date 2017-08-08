829f5cb15df9f9121f39d67fa40c44362997edc9
anchao <anchao@mininglamp.com>
2017-08-08 17:13:06 +0800

    丰富路由配置

--- scripts/main/views/HeaderView.js
+++ scripts/main/views/HeaderView.js
@@ -1,27 +1,27 @@
 /**
  * Created by anchao on 2016/6/29.
  */
-import {React,NavLink} from '../../common/Util';
+import {React, NavLink} from '../../common/Util'
 
 class HeaderView extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor (props) {
+    super(props)
+  }
 
-    render() {
-        return (
-            <div id="header" className="clearfix">
-                <div className="pull-left" id="logo"></div>
-                <div className="pull-left navigation">
-                    <ul role="nav" className="list-unstyled list-inline">
-                        <li><NavLink replace to="/login" activeClassName="active">登录</NavLink></li>
-                        <li><NavLink replace to="/app/todos" activeClassName="active">示例</NavLink></li>
-                    </ul>
-                </div>
-                <div className="pull-right"></div>
-            </div>
-        );
-    }
+  render () {
+    return (
+      <div id='header' className='clearfix'>
+        <div className='pull-left' id='logo' />
+        <div className='pull-left navigation'>
+          <ul role='nav' className='list-unstyled list-inline'>
+            <li><NavLink replace to='/login' activeClassName='active'>登录</NavLink></li>
+            <li><NavLink replace to='/app/todos' activeClassName='active'>示例</NavLink></li>
+          </ul>
+        </div>
+        <div className='pull-right' />
+      </div>
+    )
+  }
 }
 
-export default HeaderView;
+export default HeaderView
