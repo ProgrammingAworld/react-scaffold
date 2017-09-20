9358bfc2365420fb6740efdf0da0119c03a19e17
anchao <pvfhv@qq.com>
2017-09-20 13:49:53 +0800

    webpack-dev-server增加Inline热替换

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,45 @@
 /**
  * Created by anchao on 2016/5/12.
  */
-var path = require('path');
+var path = require('path')
+var webpack = require('webpack')
+
 module.exports = {
-    entry: [
-        'webpack/hot/dev-server',
-        'webpack-dev-server/client?http://localhost:3000',
-        path.resolve(__dirname, 'scripts/app.js')
-    ],
-    resolve: {
-        extensions: ['', '.js', '.jsx']
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: "/scripts/",
-        filename: 'app.js',
-        sourceMapFilename: 'app.map'
-    },
-    devtool: "source-archives",
-    module: {
-        loaders: [{
-            test: /\.jsx?$/,
-            loader: 'babel',
-            query: {
-                cacheDirectory: true,
-                presets: ['env', 'stage-0', 'react'],
-                compact: false
-            }
-        }]
-    }
-};
+  entry: [
+    'webpack-dev-server/client?http://0.0.0.0:3333',
+    'webpack/hot/only-dev-server',
+    path.resolve(__dirname, 'scripts/app.js')
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/scripts/',
+    filename: 'app.js',
+    sourceMapFilename: 'app.map'
+  },
+  devtool: 'source-archives',
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loader: ['babel'],
+      query: {
+        cacheDirectory: true,
+        presets: ['env', 'stage-0', 'react'],
+        compact: false
+      }
+    }]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    host: '0.0.0.0',
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3333,
+    inline: true,
+    hot: true
+  }
+}
