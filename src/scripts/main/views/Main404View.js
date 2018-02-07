4a4b911011af238a9c9466b2b97ad32113382ef3
anchao <pvfhv@qq.com>
2018-02-07 20:51:38 +0800

    🍭优化组件

--- src/scripts/main/views/Main404View.js
+++ src/scripts/main/views/Main404View.js
@@ -2,22 +2,19 @@
  * Created by anchao on 2016/7/26.
  */
 import { React } from 'common/Util'
-import ReactComponentBase from 'base/ReactComponentBase'
 import HeaderView from './HeaderView'
 
-class Main404View extends ReactComponentBase {
-    render() {
-        return (
-            <div id="chief">
-                <HeaderView />
-                <div className="main404">
-                    <h1>not found</h1>
-                    <h2>does not exist. time to go outside.</h2>
-                    <div><a href="#/">返回</a></div>
-                </div>
+function Main404View() {
+    return (
+        <div id="chief">
+            <HeaderView />
+            <div className="main404">
+                <h1>not found</h1>
+                <h2>does not exist. time to go outside.</h2>
+                <div><a href="#/">返回</a></div>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default Main404View
