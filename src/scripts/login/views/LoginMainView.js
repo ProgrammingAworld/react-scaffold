27351492bc497417d00aa31fb5439bd3e28a0dd0
anchao <anchao@mininglamp.com>
2018-02-07 10:23:49 +0800

    增加兼容钩子脚本

--- src/scripts/login/views/LoginMainView.js
+++ src/scripts/login/views/LoginMainView.js
@@ -162,6 +162,9 @@ class LoginMainView extends ReactComponentBase {
       // 清空错误信息
       setError('')
       
+      
+      
+      
       // 登录检验
       login({ data: { username, pwd, type } }).done((res) => {
           if (res.statusCode === 200) {
