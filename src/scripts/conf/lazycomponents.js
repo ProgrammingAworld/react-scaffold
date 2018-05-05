fbdc0575b0b0b86cdc531f5bc748480bfee08a9c
anchao <pvfhv@qq.com>
2018-05-05 08:35:04 +0800

    优化lazyload

--- src/scripts/conf/lazycomponents.js
+++ src/scripts/conf/lazycomponents.js
@@ -7,47 +7,18 @@
 import Loadable from 'react-loadable'
 import ComLoading from 'common/views/ComponentLoading'
 
-const lazyloadDefault = {
-    loading: ComLoading
-}
+const lazyload = importUrl => Loadable({
+    loading: ComLoading,
+    loader: () => importUrl
+})
 
-export const LoginView = Loadable({
-    ...lazyloadDefault,
-    loader: () => import('root/login/views/container')
-})
-export const MainAppView = Loadable({
-    ...lazyloadDefault,
-    loader: () => import('common/views/MainAppView')
-})
-export const TodoMainView = Loadable({
-    ...lazyloadDefault,
-    loader: () => import('root/todos/views/components')
-})
-export const ComponentMainView = Loadable({
-    ...lazyloadDefault,
-    loader: () => import('root/components/views/components')
-})
-export const TableMainView = Loadable({
-    ...lazyloadDefault,
-    loader: () => import('root/components/views/components/TableExampleView')
-})
-export const CodeEditorView = Loadable({
-    ...lazyloadDefault,
-    loader: () => import('root/components/views/components/AceExampleView')
-})
-export const BootstrapView = Loadable({
-    ...lazyloadDefault,
-    loader: () => import('root/components/views/components/BootstrapReactView')
-})
-export const ValidateView = Loadable({
-    ...lazyloadDefault,
-    loader: () => import('root/components/views/components/ValidatorView')
-})
-export const AntdView = Loadable({
-    ...lazyloadDefault,
-    loader: () => import('root/components/views/components/AntdView')
-})
-export const NotFindView = Loadable({
-    ...lazyloadDefault,
-    loader: () => import('common/views/Main404View')
-})
+export const LoginView = lazyload(import('root/login/views/container'))
+export const MainAppView = lazyload(import('common/views/MainAppView'))
+export const TodoMainView = lazyload(import('root/todos/views/components'))
+export const ComponentMainView = lazyload(import('root/components/views/components'))
+export const TableMainView = lazyload(import('root/components/views/components/TableExampleView'))
+export const CodeEditorView = lazyload(import('root/components/views/components/AceExampleView'))
+export const BootstrapView = lazyload(import('root/components/views/components/BootstrapReactView'))
+export const ValidateView = lazyload(import('root/components/views/components/ValidatorView'))
+export const AntdView = lazyload(import('root/components/views/components/AntdView'))
+export const NotFindView = lazyload(import('common/views/Main404View'))
