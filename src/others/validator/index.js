b053455f31029cca6d648ee1534ddfaf031d438e
anchao <pvfhv@qq.com>
2018-08-02 19:03:35 +0800

    fix(validator): modify validator component

--- src/others/validator/index.js
+++ src/others/validator/index.js
@@ -1,12 +1,46 @@
 import { React } from 'framework/Util'
+import ReactComponentBase from 'base/ReactComponentBase'
 import './scss/index.scss'
 
 function Validator() {
     return (
         <div className="validatormain-others">
-            验证组件
+            <Validator.TabTitle />
+            <Validator.TabContent />
         </div>
     )
+}
+
+Validator.TabTitle = class TabTitle extends ReactComponentBase {
+    constructor(props){
+        super(props)
+        
+        this.state = {
+            title: '验证标题'
+        }
+    }
+    
+    render(){
+        return (
+            <div>{this.state.title}</div>
+        )
+    }
+}
+
+Validator.TabContent = class TabContent extends ReactComponentBase {
+    constructor(props){
+        super(props)
+        
+        this.state = {
+            content: '这里是一些内容'
+        }
+    }
+    
+    render(){
+        return (
+            <div>{this.state.content}</div>
+        )
+    }
 }
 
 export default Validator
